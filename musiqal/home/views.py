ea7fb24ac90e7f71d2c9537f293cffdd14793020
niftyNitin <gupta10nitin99@gmail.com>
2022-10-29 22:59:38 +0530

    API testing

--- musiqal/home/views.py
+++ musiqal/home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
+import requests
+import json
 
 # Create your views here.
 def index(request):
@@ -47,3 +49,33 @@ def signUp(request):
             return redirect('login')
     
     return render(request, 'register.html')
+    
+def search(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    search_query = request.GET.get('search')
+    # send search_query to the api call
+    url_search = "https://youtube-music1.p.rapidapi.com/v2/search"
+    querysearch = {"query":search_query}
+
+    headers = {
+        "X-RapidAPI-Key": "fe4c8a33f4mshb03efeb944d6fb5p1954e8jsna74e41f4c88c",
+        "X-RapidAPI-Host": "youtube-music1.p.rapidapi.com"
+    }
+    
+    response = requests.get(url_search, headers=headers, params=querysearch)
+    song_id = json.loads(response.text)['result']['songs'][0]['id']
+    
+    # send song_id to the api call to download song
+    # url_download = "https://youtube-music1.p.rapidapi.com/get_download_url"
+    # querydownload = {"id":song_id,"ext":"mp3"}
+    
+    # response = requests.get(url_download, headers=headers, params=querydownload)
+    # download_url = json.loads(response.text)['result']['download_url']
+    # mp3 = requests.get(download_url)
+    # with open('song.mp3', 'wb') as f:
+    #     f.write(mp3.content)
+        
+    return render(request, 'search.html')
+        
+    
